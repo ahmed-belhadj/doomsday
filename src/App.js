bc03ceec338bd94a50e285290c271e10286e75e0
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-06-24 13:17:11 -0500

    Added sfc list and imported into App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import List from "./List";
 import "./App.css";
 
 class App extends Component {
@@ -11,7 +12,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>> Hello </h1>
+        <List list={this.state.list} />{" "}
       </div>
     );
   }
---
+++ src/List.js
@@ -0,0 +1,6 @@
+import React from "react";
+const List = props => {
+  return <div>hello world</div>;
+};
+
+export default List;
